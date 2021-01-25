1096d1f280526f5748cd3bf2d96d6af8a9029876
ikenagashunta <e18k23@gmail.com>
2021-01-25 16:37:38 +0900

    key属性を数字だけに変更。

--- src/components/cellsTable/TableRenderer.tsx
+++ src/components/cellsTable/TableRenderer.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 type Props = {
-    cellsBord: boolean[][]
+  cellsBord: boolean[][]
 }
 
 const TableRenderer: React.FC<Props> = ({ cellsBord }) => {
   const tbody = cellsBord.map((row, y) => {
-    const yIndex = `row-${y}`;
+    const yIndex = `${y}`;
     const cellsRow = row.map((cell, x) => {
-      const xIndex = `col-${x}`;
+      const xIndex = `${x}`;
       return (cell ? <td key={xIndex} className="living" /> : <td key={xIndex} />);
     });
     return <tr key={yIndex}>{cellsRow}</tr>;
